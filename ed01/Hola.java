eb673f2960d06c696199b8cdf034be46c22e7468
Rafael Antonio Minaya <rafael_mr20@outlook.com>
2021-11-25 20:35:30 -0500

    Añadimos la primera carpeta junto con la primera clase

---
+++ ed01/Hola.java
@@ -0,0 +1,14 @@
+class Hola {
+    public static void main(String ...args){
+        //Declaración de variables
+        int primerNumero;
+        int segundoNumero;
+        int resultado;
+
+        //Asignación de variables
+        primerNumero = 13;
+        segundoNumero = 34;
+        resultado = primerNumero + segundoNumero;
+        System.out.println(resultado);
+    }
+}
